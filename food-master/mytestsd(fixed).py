79c71b43767093fa2fd207b6dbcf98a1eda42985
Pmir1707 <56644723+Pmir1707@users.noreply.github.com>
2019-10-17 21:27:34 +0800

    Add files via upload

---
+++ food-master/mytestsd(fixed).py
@@ -0,0 +1,70 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot("944485905:AAHrw7jtHjnAVxqU7GsPS_xrhPPO6fUdiqU")
+
+@bot.message_handler(commands=["start"])
+def start(m):
+    msg = bot.send_message(m.chat.id, "Hello")
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*[types.KeyboardButton(name) for name in ['Меню', 'Акции']])
+    keyboard.add(*[types.KeyboardButton(name) for name in ['Поиск по ингредиентам', 'Расчёт калорий']])
+    keyboard.add(*[types.KeyboardButton(name) for name in ['Корзина']])
+    bot.send_message(m.chat.id, 'Выберите нужный пункт меню', reply_markup=keyboard)
+    bot.register_next_step_handler(msg, name)
+
+def name(m):
+    if m.text == 'Меню':
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert1) for advert1 in ['Еда']])
+        keyboard.add(*[types.KeyboardButton(advert1) for advert1 in ['Напитки']])
+        keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
+        bot.send_message(m.chat.id, 'Еда или напитки?', reply_markup=keyboard)
+    elif m.text == 'Акции':
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert2) for advert2 in ['В разработке']])
+        keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
+        bot.send_message(m.chat.id, 'Акций к сожалению нет :(', reply_markup=keyboard)
+    elif m.text == 'Поиск по ингредиентам': #####делаю поиск по ингредиентам
+        msg = bot.send_message(m.chat.id, "Отлично")
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert3) for advert3 in ['Пицца', 'Суши']])
+        keyboard.add(*[types.KeyboardButton(advert3) for advert3 in ['Первое блюдо', 'Второе блюдо']])
+        keyboard.add(*[types.KeyboardButton(advert3) for advert3 in ['Салат']])
+        keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
+        bot.send_message(m.chat.id, 'Что будем искать?', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, advert3)
+    elif m.text == 'Расчёт калорий':
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert4) for advert4 in ['Выбрать категорию']])
+        keyboard.add(*[types.KeyboardButton(advert4) for advert4 in ['Выбрать блюдо']])
+        keyboard.add(*[types.KeyboardButton(advert4) for advert4 in ['Рассчитать и вывести']])
+        keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
+        bot.send_message(m.chat.id, 'Что нужно сделать?', reply_markup=keyboard)
+    elif m.text == 'Корзина':
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert5) for advert5 in ['Оформить заказ']])
+        keyboard.add(*[types.KeyboardButton(advert5) for advert5 in ['Убрать некоторую еду из корзины']])
+        keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
+        bot.send_message(m.chat.id, 'Что нужно сделать?', reply_markup=keyboard)
+#####################################################################################################
+#делаю поиск по ингредиентам
+def advert3(m):
+    if m.text == 'Пицца':
+        msg = bot.send_message(m.chat.id, "Отлично")
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Которые входят в состав']])
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Без этих ингредиентов']])
+        keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
+        bot.send_message(m.chat.id, 'Какие ингредиенты ищем?', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, advert33)
+
+def advert33(m):
+    if m.text == 'Которые входят в состав':
+            bot.send_message(m.chat.id,'Провожу поиск...')
+            
+    if m.text == 'Без этих ингредиентов':
+            bot.send_message(m.chat.id,'Провожу поиск...')
+            
+#####################################################################################################
+bot.polling()
