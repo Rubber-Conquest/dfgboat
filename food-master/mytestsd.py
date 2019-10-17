d3a9034e8cbef619c6088fcc2616fd33ca0ed2be
sjv4sh <56645094+sjv4sh@users.noreply.github.com>
2019-10-18 00:37:31 +0800

    Add files via upload

--- food-master/mytestsd.py
+++ food-master/mytestsd.py
@@ -3,6 +3,10 @@ from telebot import types
 
 bot = telebot.TeleBot("944485905:AAHrw7jtHjnAVxqU7GsPS_xrhPPO6fUdiqU")
 
+pizza_menu='1. Пепперони и томаты\n2. Сырная\n3. Супер Мит\n4. Морская с тунцом\n5. Вегетарианская\n6. Грибная пепперони\n7. Грибная\n8. Три сыра\n\nКакая пицца вас заинтересовала?'
+sushi_menu='1. Ролл Хатамото\n2. Ролл с тигровой креветкой и лососем\n3. Ролл Сочная креветка\n4. Ролл Берри\n5. Ролл Калифорния в кунжуте\n6. Ролл Калифорния с креветкой\n\nКакие роллы вас заинтересовали?'
+
+
 @bot.message_handler(commands=["start"])
 def start(m):
     msg = bot.send_message(m.chat.id, "Hello")
@@ -15,17 +19,21 @@ def start(m):
 
 def name(m):
     if m.text == 'Меню':
+        msg = bot.send_message(m.chat.id, "Отлично")
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*[types.KeyboardButton(advert1) for advert1 in ['Еда']])
         keyboard.add(*[types.KeyboardButton(advert1) for advert1 in ['Напитки']])
         keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
         bot.send_message(m.chat.id, 'Еда или напитки?', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, menu_menu)
+        
     elif m.text == 'Акции':
+        msg = bot.send_message(m.chat.id, "Отлично")
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*[types.KeyboardButton(advert2) for advert2 in ['В разработке']])
         keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
         bot.send_message(m.chat.id, 'Акций к сожалению нет :(', reply_markup=keyboard)
-    elif m.text == 'Поиск по ингредиентам': #####делаю поиск по ингредиентам
+    elif m.text == 'Поиск по ингредиентам': 
         msg = bot.send_message(m.chat.id, "Отлично")
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*[types.KeyboardButton(advert3) for advert3 in ['Пицца', 'Суши']])
@@ -35,6 +43,7 @@ def name(m):
         bot.send_message(m.chat.id, 'Что будем искать?', reply_markup=keyboard)
         bot.register_next_step_handler(msg, advert3)
     elif m.text == 'Расчёт калорий':
+        msg = bot.send_message(m.chat.id, "Отлично")
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*[types.KeyboardButton(advert4) for advert4 in ['Выбрать категорию']])
         keyboard.add(*[types.KeyboardButton(advert4) for advert4 in ['Выбрать блюдо']])
@@ -42,59 +51,83 @@ def name(m):
         keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
         bot.send_message(m.chat.id, 'Что нужно сделать?', reply_markup=keyboard)
     elif m.text == 'Корзина':
+        msg = bot.send_message(m.chat.id, "Отлично")
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*[types.KeyboardButton(advert5) for advert5 in ['Оформить заказ']])
         keyboard.add(*[types.KeyboardButton(advert5) for advert5 in ['Убрать некоторую еду из корзины']])
         keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
         bot.send_message(m.chat.id, 'Что нужно сделать?', reply_markup=keyboard)
-#####################################################################################################
-#делаю поиск по ингредиентам
+        
 def advert3(m):
     if m.text=='Назад':
-        msg = bot.send_message(m.chat.id, "Отлично")
-        bot.register_next_step_handler(msg, start)
-    elif m.text == 'Пицца':
+        msg = bot.send_message(m.chat.id, "Выполняю переход назад...")
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in ['Меню', 'Акции']])
+        keyboard.add(*[types.KeyboardButton(name) for name in ['Поиск по ингредиентам', 'Расчёт калорий']])
+        keyboard.add(*[types.KeyboardButton(name) for name in ['Корзина']])
+        bot.send_message(m.chat.id, 'Выберите нужный пункт меню', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, name)
+    if m.text == 'Пицца':
         msg = bot.send_message(m.chat.id, "Отлично")
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Которые входят в состав']])
         keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Без этих ингредиентов']])
         keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
         m=bot.send_message(m.chat.id, 'Какие ингредиенты ищем?', reply_markup=keyboard)
-        bot.register_next_step_handler(msg, advert33)
-
-def advert33(m):
-    if m.text == 'Которые входят в состав':
+   
+def menu_menu(m):
+    if m.text=='Назад':
+        msg = bot.send_message(m.chat.id, "Выполняю переход назад...")
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in ['Меню', 'Акции']])
+        keyboard.add(*[types.KeyboardButton(name) for name in ['Поиск по ингредиентам', 'Расчёт калорий']])
+        keyboard.add(*[types.KeyboardButton(name) for name in ['Корзина']])
+        bot.send_message(m.chat.id, 'Выберите нужный пункт меню', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, name)
+    if m.text=='Еда':
         msg = bot.send_message(m.chat.id, "Отлично")
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Соус томатный','Соус цезарь']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Соус грибной','Соус сырный']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Креметте','Шампиньоны']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Сыр Моцарелла','Сыр Пармезан']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Пепперони','Ветчина','Бекон','Салями']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Цыплёнок','Орегано','Лук']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Томаты','Базилик','Маслины']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Тунец','Перец',]])
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Пицца', 'Суши', 'Салат']])
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Первое блюдо', 'Второе блюдо']])
         keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
-        bot.send_message(m.chat.id,'Выберете ингредиент')
-        bot.register_next_step_handler(msg, find_food)
-            
-    if m.text == 'Без этих ингредиентов':
+        m=bot.send_message(m.chat.id, 'Что желаете заказать?', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, eat_menu)
+    if m.text=='Напитки':
         msg = bot.send_message(m.chat.id, "Отлично")
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Соус томатный','Соус цезарь']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Соус грибной','Соус сырный']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Креметте','Шампиньоны']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Сыр Моцарелла','Сыр Пармезан']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Пепперони','Ветчина','Бекон','Салями']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Цыплёнок','Орегано','Лук']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Томаты','Базилик','Маслины']])
-        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Тунец','Перец',]])
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Чай', 'Вода']])
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Сок', 'Газировка']])
         keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
-        bot.send_message(m.chat.id,'Выберете ингредиент')
-        bot.register_next_step_handler(msg, find_food)
+        m=bot.send_message(m.chat.id, 'Что желаете заказать?', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, drink_menu)
 
-def find_food(m):
-    bot.send_message(m.chat.id,'Произвожу анализ...')
+def eat_menu(m):
+    if m.text=='Назад':
+        msg = bot.send_message(m.chat.id, "Выполняю переход назад...")
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert1) for advert1 in ['Еда']])
+        keyboard.add(*[types.KeyboardButton(advert1) for advert1 in ['Напитки']])
+        keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
+        bot.send_message(m.chat.id, 'Еда или напитки?', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, menu_menu)
+    if m.text=='Пицца':
+        msg = bot.send_message(m.chat.id, pizza_menu)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Пепперони и томаты', 'Сырная']])
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Супер Мит', 'Морская с тунцом']])
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Вегетарианская', 'Грибная пепперони']])
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Грибная', 'Три сыра']])
+        keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
+        bot.register_next_step_handler(msg, about_pizza)
+    if m.text=='Суши':
+        msg = bot.send_message(m.chat.id, sushi_menu)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Ролл Хатамото', 'Ролл с тигровой креветкой и лососем']])
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Ролл Сочная креветка', 'Ролл Берри']])
+        keyboard.add(*[types.KeyboardButton(advert33) for advert33 in ['Ролл Калифорния в кунжуте', 'Ролл Калифорния с креветкой']])
+        keyboard.add(*[types.KeyboardButton(advert0) for advert0 in ['Назад']])
+        bot.register_next_step_handler(msg, about_sushi)
+
+
             
-#####################################################################################################
-bot.polling()
+bot.polling(none_stop=True)
